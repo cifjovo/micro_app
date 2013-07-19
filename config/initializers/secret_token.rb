8c25b57cae2fa59366fdd1c48c658249bb17c611
RoR Student <ror-student@mll-pc-14.(none)>
2013-07-19 15:25:07 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MacroApp::Application.config.secret_token = '0438a8a7538602e2b77e4efa66245097d36643c35f68b6857247c767138d56476fe0e3128d292afec4364ef76e16b50516e4d1ffcac392497bc94dd1ad0a13e2'
